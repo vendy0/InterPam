b3daa2ffb12dd9d104d757563dbfedd2c5e6ea1f
Vendy A15 <andyvenson99@gmail.com>
2026-01-02 07:02:34 -0500

    feat(assets): ajout d'icônes pour l'application et le badge

--- commit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "--- Générateur de Commit ---"
-read -p "Type (feat/fix/chore): " TYPE
-read -p "Scope (ex: admin/pari): " SCOPE
-read -p "Message: " MSG
-
-git commit -m "$TYPE($SCOPE): $MSG"
-
