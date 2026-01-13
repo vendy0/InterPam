eacc63307afadefa004f980400192877825e062b
Vendy A15 <andyvenson99@gmail.com>
2026-01-13 02:30:40 -0500

    Quelque corrections mineures.

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,16 +3,16 @@ import multiprocessing
 
 # 1. Adresse et Port
 # '0.0.0.0' permet d'écouter sur toutes les interfaces (utile pour Docker/Twilight Paradox)
-bind = "0.0.0.0:8000"
+bind = "0.0.0.0:2037"
 
 # 2. Performance & Workers
 # Formule standard : (2 x nombre de coeurs CPU) + 1
 # Pour InterPam, cela permet de gérer les calculs de cotes en parallèle sans bloquer l'UI
 workers = multiprocessing.cpu_count() * 2 + 1
 
-# Type de worker : 'sync' est très stable. 
+# Type de worker : 'sync' est très stable.
 # Si vous avez beaucoup de requêtes longues (API), 'gevent' est une alternative.
-worker_class = 'sync'
+worker_class = "sync"
 
 # 3. Sécurité & Timeouts
 # 30 secondes suffisent largement pour traiter un pari ou valider une invitation
@@ -21,14 +21,14 @@ keepalive = 2
 
 # 4. Journalisation (Logging)
 # Indispensable pour débugger les erreurs de data.py en production
-accesslog = '-' # '-' signifie sortie standard (stdout)
-errorlog = '-'
-loglevel = 'info'
+accesslog = "-"  # '-' signifie sortie standard (stdout)
+errorlog = "-"
+loglevel = "info"
 
 # 5. Process Naming
 # Pratique pour identifier le processus dans 'top' ou 'htop' sur le serveur
-proc_name = 'interpam_gunicorn'
+proc_name = "interpam_gunicorn"
 
 # 6. Gestion du code
 # Relance automatiquement le serveur si le code change (utile uniquement en dev/staging)
-# reload = True 
+# reload = True
